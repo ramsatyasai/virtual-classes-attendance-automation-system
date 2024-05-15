16bff7197f0ee16675f7f6a073ee30d7004635b4
Kapileswarapu Ram Satya Sai <ramsatyasai9707@gmail.com>
2024-05-15 13:15:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def generate_report():
         return render_template('index.html', error=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
